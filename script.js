f07497f4a9b6cbab58cead87030c9f7fa76b6338
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-01 20:04:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const checkboxes = document.querySelectorAll('.toggle');
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+        // Filter the checkboxes to find the ones that are checked
+        const checkedCheckboxes = Array.from(checkboxes).filter(cb => cb.checked);
+
+        // If more than 2 checkboxes are checked, uncheck the first one checked
+        if (checkedCheckboxes.length > 2) {
+            checkedCheckboxes[0].checked = false;
+        }
+    });
+});
